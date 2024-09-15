28cc941aa8fcb55d3805b65799861e780dfcaf21
allrabani <allrabanihafiz@gmail.com>
2024-09-16 06:02:03 +0700

    menambahkan search dan detail karya

--- js/script.js
+++ js/script.js
@@ -1,15 +1,54 @@
-// Toggle class active
+// Toggle class active untuk menu
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika menu di klik
-document.querySelector("#menu").onclick = () => {
+document.querySelector("#menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// Klik di luar sidebar untuk menghilangkan nav
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik di luar elemen
 const menu = document.querySelector("#menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// Klik tombol close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
